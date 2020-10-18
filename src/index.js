80888d096518717637fcf07ea432c1850ec97c21
Germán Parisi <germannparisi@gmail.com>
2020-10-18 17:49:41 -0300

    Agrego estructura de app react

--- src/index.js
+++ src/index.js
@@ -3,11 +3,95 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Cuerpo extends React.Component {
+    render() {
+        return  (
+            <Col></Col>
+        );
+    }
+}
+
+class Menu extends React.Component {
+    render() {
+        return (
+            <Col></Col>
+        );
+    }
+}
+
+class Conetenedor extends React.Component {
+    render() {
+        return <Container fluid>
+            <Row className="menu-lateral">
+                <Menu />
+            </Row>
+        </Container>;
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Conetenedor />,
   document.getElementById('root')
 );
 
